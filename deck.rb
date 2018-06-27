4c74e3429044d3bcde6b65cde560588030c933ff
savchenko <mr.syava1@gmail.com>
2018-06-27 11:41:23 +0500

    Норм колода

--- deck.rb
+++ deck.rb
@@ -1,7 +1,7 @@
 require_relative 'card.rb'
 
 class Deck
-  CARDS = %w[K K A A].freeze #2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 
+  CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'].freeze
   SUITS = ['♥', '♣', '♦', '♠'].freeze
   def initialize
     @deck = create_deck
